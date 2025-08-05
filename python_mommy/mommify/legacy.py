b28cc54f5bfb671d203554a11c68edf724b5c1db
Acute Interpreter Panic <223899499+acute-interpreter-panic@users.noreply.github.com>
2025-08-05 11:20:47 +0200

    using util function in legacy

--- python_mommy/mommify/legacy.py
+++ python_mommy/mommify/legacy.py
@@ -1,12 +1,13 @@
+from typing import Optional
 from pathlib import Path
 import stat
 import logging
 import subprocess
 import sys
 
-from .. import mommy
-from ..config import load_config
+
 from ..static import MOMMY
+from .utils import find_venv_dir
 
 
 mommy_logger = logging.getLogger("mommy")
@@ -110,7 +111,11 @@ def _install_pip_hook(path: Path):
         f.write(text)
 
 
-def mommify():
+def mommify(venv_dir: Optional[Path] = None):
+    venv_dir = find_venv_dir(venv_dir=venv_dir)
+    if venv_dir is None:
+        mommy_logger.error("%s couldn't find a venv directory to mess up~", MOMMY.ROLE)
+        serious_logger.error("couldn't find a venv directory")
     bin_path = Path(sys.prefix, "bin").resolve()
 
     mommy_logger.info("%s looks in %s to mess %s %s system up~ <33", MOMMY.ROLE, MOMMY.PRONOUN, MOMMY.YOU, str(bin_path))
