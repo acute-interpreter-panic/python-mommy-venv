b62665fa141b866748f59f2ac693972b91c8a8c1
Acute Interpreter Panic <223899499+acute-interpreter-panic@users.noreply.github.com>
2025-08-05 10:53:00 +0200

    improved logging config

--- python_mommy/__main__.py
+++ python_mommy/__main__.py
@@ -1,29 +1,53 @@
-from pathlib import Path
-import logging
-import sys
 import argparse
 
-from . import mommy
-from .config import load_config
 from .static import MOMMY
-from .mommify import legacy_mommify, mommify
-
-log_level = logging.INFO
-
-mommy_logger = logging.getLogger("mommy")
-mommy_logger.setLevel(logging.INFO)
-serious_logger = logging.getLogger("serious")
-serious_logger.setLevel(50)
 
 
-def _config_logging(verbose: bool):
+
+def _config_logging(verbose: bool, verbose_all: bool):
+    # https://stackoverflow.com/a/59705351/16804841
+    import logging
+
+    if verbose_all:
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+
+
+    mommy_logger = logging.getLogger("mommy")
+    serious_logger = logging.getLogger("serious")
+
+    def get_mommy_handler():
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s~')
+        handler.setFormatter(formatter)
+        return handler
+
+    mommy_logger.addHandler(get_mommy_handler())
+
+
+    def get_serious_handler():
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(levelname)s:%(name)s:\t%(message)s")
+        handler.setFormatter(formatter)
+        return handler
+
+    serious_logger.addHandler(get_serious_handler())
     if verbose:
-        logging.getLogger().setLevel(logging.DEBUG)
-        mommy_logger.setLevel(50)
         serious_logger.setLevel(logging.DEBUG)
+        mommy_logger.setLevel(50)
+    else:
+        serious_logger.setLevel(50)
+        mommy_logger.setLevel(logging.INFO)
+
 
 def _assert_venv(only_warn: bool = False):
+    import sys
+
     if sys.prefix == sys.base_prefix:
+        import logging
+        mommy_logger = logging.getLogger("mommy")
+        serious_logger = logging.getLogger("serious")
+
         mommy_logger.error("%s doesn't run in a virtual environment~", MOMMY.ROLE)
         serious_logger.error("this should run in a virtual environment")
         if not only_warn:
@@ -33,7 +57,7 @@ def _assert_venv(only_warn: bool = False):
 def mommify_venv(is_mommy: bool = True):
     MOMMY.set_roles(is_mommy)
 
-    parser = argparse.ArgumentParser(description="patch the virtual environment to use mommy")
+    parser = argparse.ArgumentParser(description=f"patch the virtual environment which will use {MOMMY.ROLE}")
 
     parser.add_argument(
         "-v", "--verbose",
@@ -42,17 +66,23 @@ def mommify_venv(is_mommy: bool = True):
     )
 
     parser.add_argument(
+        "--verbose-all",
+        action="store_true",
+        help="enables serious output and verbose output for every library"
+    )
+
+    parser.add_argument(
         '--you', 
         type=str, 
         default='girl', 
         nargs='?',
-        help='how do you want mommy to call you?'
+        help=f'how do you want {MOMMY.ROLE} to call you?'
     )
 
     parser.add_argument(
         "-r", "--no-requests",
         action="store_true",
-        help="by default if makes one request to GitHub to fetch the newest responses, this disables that"
+        help=f"by default {MOMMY.ROLE} makes one request to GitHub to fetch the newest responses, this disables that"
     )
 
     parser.add_argument(
@@ -64,10 +94,11 @@ def mommify_venv(is_mommy: bool = True):
 
     args = parser.parse_args()
 
+    _config_logging(args.verbose or args.verbose_all, args.verbose_all)
     MOMMY.YOU = args.you
-    _config_logging(args.verbose)
     _assert_venv()
 
+    from .mommify import legacy_mommify, mommify
     if args.legacy:
         legacy_mommify()
     else:
@@ -79,4 +110,7 @@ def daddify_venv():
 
 # run as module
 if __name__ == "__main__":
+    from . import mommy
+
+    _config_logging(verbose=False, verbose_all=False)
     mommy()
--- python_mommy/mommify/source.py
+++ python_mommy/mommify/source.py
@@ -1,9 +1,21 @@
+from typing import Optional
 import logging
+import sys
+from pathlib import Path
+
+from .utils import find_venv_dir
 
 
 mommy_logger = logging.getLogger("mommy")
 serious_logger = logging.getLogger("serious")
 
 
-def mommify():
+
+
+
+def mommify(venv_dir: Optional[Path] = None):
     print("normal mommify")
+
+    venv_dir = find_venv_dir(venv_dir=venv_dir)
+    serious_logger.info("using venv dir %s", venv_dir)
+    
